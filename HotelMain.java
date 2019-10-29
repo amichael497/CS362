a165c8c071588124c92d0f9acb8f85bd4852bc8e
amichael497 <52112106+amichael497@users.noreply.github.com>
2019-10-29 15:59:08 -0500

    Add files via upload

---
+++ HotelMain.java
@@ -0,0 +1,17 @@
+
+public class HotelMain {
+
+	public static void main(String[] args) {
+		loginScreen();
+
+	}
+	
+	public static void loginScreen() {
+		
+	}
+	
+	public static void mainScreen() {
+		
+	}
+
+}
